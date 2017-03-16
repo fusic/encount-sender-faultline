e55b4a266079cac49aa382601d161d8995f13ef7
k1LoW <k1lowxb@gmail.com>
2017-03-16 11:31:51 +0900

    Initial commit

---
+++ src/Sender/Faultline.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace EncountSenderFaultline\Sender;
+
+use Encount\Sender\SenderInterface;
+use Encount\Collector\EncountCollector;
+use Faultline\Instance;
+
+class Faultline implements SenderInterface
+{
+    /**
+     * send.
+     */
+    public function send($config, EncountCollector $collector)
+    {
+        $exc = new \Airbrake\Errors\Base($collector->description, array_slice(debug_backtrace(), 2));
+        $notice = Instance::buildNotice($exc);
+        // type
+        $notice['errors'][0]['type'] = $collector->errorType;
+        // component
+        // action
+        $params = $collector->requestParams;
+        if (!empty($params['params']['controller'])) {
+            $notice['context']['component'] = $params['params']['controller'];
+        }
+        if (!empty($params['params']['action'])) {
+            $notice['context']['action'] = $params['params']['action'];
+        }
+        $notice['context']['url'] = $collector->url;
+
+        return Instance::sendNotice($notice);
+    }
+}
